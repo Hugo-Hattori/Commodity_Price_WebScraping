70ec1101b6e438ddc7f3263d8b47bd275862b75e
Hugo Massashi Hatori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-06-28 19:26:27 -0300

    Update Commodity_Price_Analysis.py

--- Commodity_Price_Analysis.py
+++ Commodity_Price_Analysis.py
@@ -1,26 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #Passo 1: Abrir o navegador
 from selenium import webdriver
-
 navegador = webdriver.Chrome()
-
-
-# In[2]:
-
 
 #Passo 2: Importar a base de dados
 import pandas as pd
-
 tabela = pd.read_excel("commodities.xlsx")
-
-
-# In[3]:
-
 
 #Passo 3: Pesquisar os preços atuais (cotação atual)
 for linha in tabela.index:
@@ -35,26 +19,11 @@ for linha in tabela.index:
     
     #Passo 4: Atualizar os preços na base de dados
     tabela.loc[linha,"Preço Atual"] = float(preco)
-
 navegador.quit()
-
-
-# In[4]:
-
 
 #Passo 5: Criar planilha com dados atualizados e com a decisão de compra (VERDADEIRO = Compra, FALSO = Não Compra)
 tabela["Comprar"] = tabela["Preço Atual"] < tabela["Preço Ideal"]
-
-#Substituindo VERDADEIRO por Sim e FALSO por Não
-for linha in tabela.index:
-    x = tabela.loc[linha, "Comprar"]
-    if x == True:
-        x = "Sim"
-    else:
-        x = "Não"
-    tabela.loc[linha, "Comprar"] = x
-    
-    
+   
 #Criando planilha nova atualizada
 tabela.to_excel("commodities_atualizado.xlsx", index=False)
 
